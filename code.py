b51551a711b8661c7f2131ebbc96f45139559b12
Himeshnama <95176444+Himeshnama@users.noreply.github.com>
2021-11-28 20:31:12 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,24 @@
+# coin toss
+
+#favourable_outcomes = int(input("enter the number of favourable out "))
+#number_flip = int(input("pleas enter the number of times you want to flip the coin "))
+#total_outcomes = number_flip * 2
+#probability = favourable_outcomes / total_outcomes
+#print("probability is :-")
+#print(probability)
+
+
+
+#n = int(input("enter the the number you want the table of "))
+#answer = 1
+
+#while answer <= 10:
+#    print(n, 'X', answer, "=", n*answer)
+#    answer = answer + 1
+
+
+number = 100
+
+while number >= 1:
+    print(number)
+    number = number - 1
